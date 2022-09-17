8479f54526a0ac18dad6cf3d679960d05f9b8b91
NTruong <nhattruong.dev3@gmail.com>
2022-09-17 10:26:46 +0700

    fixname_folder

---
+++ resources/js/components/Admin/PayFist/PayFistComponent.vue
@@ -0,0 +1,116 @@
+<template>
+    <div class="mt-6">
+        <div class="flex justify-between">
+            <h1 class="text-[#566a7f] text-[28px] font-[700]">Tiền hàng</h1>
+            <div class="mr-5">
+                <img
+                    class="w-[32px] h-[32px] cursor-pointer"
+                    src="/images/filter.png"
+                    alt=""
+                />
+            </div>
+        </div>
+        <div class="main mt-6">
+            <table class="w-full rounded-t-md overflow-hidden bg-white">
+                <thead
+                    class="bg-[#FF3F3A] text-white h-[40px] font-bold text-[16px]"
+                >
+                    <tr class="px-2 text-left">
+                        <th class="text-center">STT</th>
+                        <th class="pl-5">NGÀY</th>
+                        <th>USER</th>
+                        <th>ĐƠN HÀNG</th>
+                        <th>LOẠI</th>
+                        <th>NỘI DUNG</th>
+                        <th>SỐ TIỀN</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                    <tr
+                        class="item border-solid border-b-[1px] border-[#E2E2E2] h-[52px] font-[16px]"
+                    >
+                        <td class="font-bold text-center">1</td>
+                        <td class="pl-5">25-5-20 03:38</td>
+                        <td>demo1</td>
+                        <td>DH-2708022</td>
+                        <td>đặt cọc</td>
+                        <td>đặt cộc cho đơn hàng 001</td>
+                        <td>1,400,000</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    setup() {},
+   
+};
+</script>
