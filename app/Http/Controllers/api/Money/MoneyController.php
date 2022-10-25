42c13f63ac1268a7d40aa556faabae7f10deba8b
Đinh Đức Dương <dinhducduong2002@outlook.com.vn>
2022-10-25 23:12:15 +0700

    fix api get money

--- app/Http/Controllers/api/Money/MoneyController.php
+++ app/Http/Controllers/api/Money/MoneyController.php
@@ -4,14 +4,27 @@ namespace App\Http\Controllers\api\Money;
 
 use App\Http\Controllers\Controller;
 use App\Models\TransactionModel;
+use App\Models\TypeTransactionModel;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 
 class MoneyController extends Controller
 {
-    public function getMoneys(){
+    public function getMoneys(Request $request){
+        $search = [
+            'from' => $request->from ? $request->from : null,
+            'to' => $request->to ? $request->to : null,
+            'username' => $request->username ? $request->username : null,
+            'status' => $request->status ? $request->status : null,
+        ];
         $model = new TransactionModel();
-        $transactions = $model->getMoneys();
-        return response()->json($transactions, Response::HTTP_OK);
+        $type_status = new TypeTransactionModel();
+        $type_transaction = $type_status->getTypeTransactions();
+        $transactions = $model->getMoneys($search);
+        $data = [
+            "transactions" => $transactions,
+            "type_transaction" => $type_transaction
+        ];
+        return response()->json($data, Response::HTTP_OK);
     }
 }
--- app/Models/TypeTransactionModel.php
+++ app/Models/TypeTransactionModel.php
@@ -4,9 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Support\Facades\DB;
 class TypeTransactionModel extends Model
 {
     protected $table = 'type_transactions';
     use HasFactory;
+
+    public function getTypeTransactions(){
+        $data = DB::table('type_transactions')->get();
+        return $data;
+    }
 }
