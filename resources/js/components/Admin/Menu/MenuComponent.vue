a345fa62b3406c4cb185a0c1f5ae5d5bc524ecb5
NTruong <nhattruong.dev3@gmail.com>
2022-12-05 21:30:32 +0700

    update

--- resources/js/components/Admin/Menu/MenuComponent.vue
+++ resources/js/components/Admin/Menu/MenuComponent.vue
@@ -1,8 +1,11 @@
 <template>
-    <div class=" mt-7 ">
+    <div class="mt-7">
         <nav class="h-[100vh]">
             <ul>
-                <li style="background-color:#E93B3B; " class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                <li
+                    style="background-color: #e93b3b"
+                    class="text-white my-2 py-2 pl-5 rounded-[10px]"
+                >
                     <router-link to="/" class="text-white">
                         <font-awesome-icon icon="fa fa-home" />
                         Trang chủ
@@ -14,26 +17,38 @@
                         Đơn Hàng
                     </router-link>
                 </li>
-                <li v-if="role == 1 || role == 2" class="text-white my-2 py-2 pl-5 rounded-[10px]">
-                    <router-link to="/pay-fist" class="text-white">
+                <li class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                    <router-link
+                        v-if="role == 1 || role == 2"
+                        to="/pay-fist"
+                        class="text-white"
+                    >
                         <font-awesome-icon icon="fa fa-money-bill" />
                         Tiền Hàng
                     </router-link>
                 </li>
-                <li v-if="role == 1" class="text-white my-2 py-2 pl-5 rounded-[10px]">
-                    <router-link to="/user" class="text-white">
+                <li class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                    <router-link v-if="role == 1" to="/user" class="text-white">
                         <font-awesome-icon icon="fas fa-users" />
                         Khách Hàng
                     </router-link>
                 </li>
-                <li v-if="role == 1 || role == 2" class="text-white my-2 py-2 pl-5 rounded-[10px]">
-                    <router-link to="chinese-money" class="text-white">
+                <li class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                    <router-link
+                        v-if="role == 1 || role == 2"
+                        to="chinese-money"
+                        class="text-white"
+                    >
                         <font-awesome-icon icon="fa fa-money-bill" />
                         Giao Dịch Tiền Trung
                     </router-link>
                 </li>
-                <li v-if="role == 1 || role == 2" class="text-white my-2 py-2 pl-5 rounded-[10px]">
-                    <router-link to="/money-vietnamese" class="text-white">
+                <li class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                    <router-link
+                        v-if="role == 1 || role == 2"
+                        to="/money-vietnamese"
+                        class="text-white"
+                    >
                         <font-awesome-icon icon="fa fa-money-bill" />
                         Giao Dịch Tiền Việt
                     </router-link>
@@ -44,8 +59,12 @@
                         Bao Hàng
                     </router-link>
                 </li>
-                <li v-if="role == 1 || role == 2" class="text-white my-2 py-2 pl-5 rounded-[10px]">
-                    <router-link to="/partner" class="text-white">
+                <li class="text-white my-2 py-2 pl-5 rounded-[10px]">
+                    <router-link
+                        v-if="role == 1 || role == 2"
+                        to="/partner"
+                        class="text-white"
+                    >
                         <font-awesome-icon icon="fa fa-handshake-alt" />
                         Đối Tác
                     </router-link>
@@ -58,21 +77,29 @@
                     </router-link>
                 </li>
 
-
                 <!-- <router-link to="/config-payment">
                        
                         Thiết Lập
                     </router-link> -->
-                <Dropdown v-if="role == 1 || role == 2" title="Thiết Lập" height="h-24" :fontAwesome="icon_fontAwesome" :icon="icon" class="text-white my-2 py-2 pl-5 rounded-[10px]" >
+                <Dropdown
+                    v-if="role == 1 || role == 2"
+                    title="Thiết Lập"
+                    height="h-24"
+                    :fontAwesome="icon_fontAwesome"
+                    :icon="icon"
+                    class="text-white my-2 py-2 pl-5 rounded-[10px]"
+                >
                     <li class="mb-2" v-if="role == 1">
-                       <router-link to="/config-payment">Thông tin thanh toán</router-link>
+                        <router-link to="/config-payment"
+                            >Thông tin thanh toán</router-link
+                        >
                     </li>
                     <li class="mb-2">
-                        <router-link to="/settings">Thiết lập cửa hàng</router-link>
+                        <router-link to="/settings"
+                            >Thiết lập cửa hàng</router-link
+                        >
                     </li>
-              
                 </Dropdown>
-
             </ul>
         </nav>
     </div>
@@ -84,50 +111,58 @@ a {
 </style>
 
 <script>
-import Dropdown from '../dropdown/Dropdown.vue'
+import Dropdown from "../dropdown/Dropdown.vue";
 import { getUser } from "../../../services/User/user";
 export default {
-   data(){
+    data() {
         return {
-            icon_fontAwesome: 'fas',
-            icon: 'fa-cog',
-            role: 0
-        }
-   },
+            icon_fontAwesome: "fas",
+            icon: "fa-cog",
+            role: 0,
+        };
+    },
     components: {
-        Dropdown
+        Dropdown,
     },
     mounted() {
-        var li = document.querySelectorAll('li');
-
-        li.forEach(el => {  
-            el.addEventListener('click', function () {
-                li.forEach(el => {
-                    el.style.backgroundColor = 'transparent'
-                })
-                el.style.backgroundColor = '#E93B3B'
-            })
+        var li = document.querySelectorAll("li");
+        li.forEach((el) => {
+            if (el.querySelector("a") == null) {
+                el.style.display = "none";
+            } else {
+                if (
+                    el.querySelector("a").getAttribute("href") ==
+                    location.pathname
+                ) {
+                    li.forEach((el) => {
+                        el.style.backgroundColor = "transparent";
+                    });
+                    el.style.backgroundColor = "#E93B3B";
+                }
+            }
+            el.addEventListener("click", function () {
+                li.forEach((el) => {
+                    el.style.backgroundColor = "transparent";
+                });
+                el.style.backgroundColor = "#E93B3B";
+            });
         });
-        this.checkRole()
+        this.checkRole();
     },
     methods: {
-        checkRole(){
+        checkRole() {
             let token = localStorage.getItem("auth_token_default") || null;
-            if(token){
-                getUser().then(res => {
-                    this.role = res.data.data.role
-                }).catch(err => {
-                    
-                })
-            }else{
-                
+            if (token) {
+                getUser()
+                    .then((res) => {
+                        this.role = res.data.data.role;
+                    })
+                    .catch((err) => {});
+            } else {
             }
-        }  
+        },
     },
-
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
