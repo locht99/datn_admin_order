80bf0ab7f5f952afd308a9b78b83e4d8626babf7
Huy20102002 <huy.dev.2010@gmail.com>
2022-12-09 22:26:36 +0700

    Fix

--- resources/js/config/config.js
+++ resources/js/config/config.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 const URL = '/api/';
-const token = window.localStorage.getItem('auth_token_default') || null;
 const config = axios.create({
     baseURL: URL,
     headers: {
         'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`
+        Authorization: `Bearer ${window.localStorage.getItem('auth_token_default')}`
     }
 })
 
