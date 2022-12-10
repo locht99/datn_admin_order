4f73e367ab011fc0eaae29dfb59e03e2f1a7a617
Đinh Đức Dương <dinhducduong2002@outlook.com.vn>
2022-12-10 15:36:58 +0700

    update

--- resources/js/config/config_ghn.js
+++ resources/js/config/config_ghn.js
@@ -4,7 +4,7 @@ const config_ghn = axios.create({
     baseURL: "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/",
     headers: {
         'Content-Type': 'application/json',
-        'ShopId': 12042,
+        'ShopId': 120427,
         token: `989b92f1-59ed-11ed-9ad7-269dd9db11fd`
     }
 })
